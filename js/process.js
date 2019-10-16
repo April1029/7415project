3cc72bef2c3313ceb8a7689f7373455fe7c613ad
April1029 <56407165+April1029@users.noreply.github.com>
2019-10-16 01:39:46 -0400

    Initial commit
    First attempt on github

---
+++ js/process.js
@@ -0,0 +1,92 @@
+$( document ).ready(function() {
+   $(".parcours-box").removeClass("parcours-active");
+   
+   
+   $(".round1").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc1").addClass("parcours-active");
+      $(".timeline-indicator").css("width","11.25%");
+   })
+   
+   $(".round2").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc2").addClass("parcours-active");
+      $(".timeline-indicator").css("width","22.70%");
+   })
+   
+   $(".round3").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc3").addClass("parcours-active");
+      $(".timeline-indicator").css("width","33.80%");
+   })
+   
+   $(".round4").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".round4").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc4").addClass("parcours-active");
+      $(".timeline-indicator").css("width","44.6%");
+   })
+   $(".round5").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".round4").css("background-color","white");
+      $(".round5").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc5").addClass("parcours-active");
+      $(".timeline-indicator").css("width","55.6%");
+   })
+   $(".round6").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".round4").css("background-color","white");
+      $(".round5").css("background-color","white");
+      $(".round6").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc6").addClass("parcours-active");
+      $(".timeline-indicator").css("width","66.9%");
+   })
+   $(".round7").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".round4").css("background-color","white");
+      $(".round5").css("background-color","white");
+      $(".round6").css("background-color","white");
+      $(".round7").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc7").addClass("parcours-active");
+      $(".timeline-indicator").css("width","78.2%");
+   })
+   $(".round8").on("click",function(){
+      $(".tl-round").css("background-color","rgb(85, 69, 69)");
+      $(".round1").css("background-color","white");
+      $(".round2").css("background-color","white");
+      $(".round3").css("background-color","white");
+      $(".round4").css("background-color","white");
+      $(".round5").css("background-color","white");
+      $(".round6").css("background-color","white");
+      $(".round7").css("background-color","white");
+      $(".round8").css("background-color","white");
+      $(".parcours-active").removeClass("parcours-active");
+      $(".parc8").addClass("parcours-active");
+      $(".timeline-indicator").css("width","89.9%");
+   })
+});

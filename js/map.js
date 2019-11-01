a2ca3d4e27f184eb2926543635069f153d68ec55
April1029 <56407165+April1029@users.noreply.github.com>
2019-11-01 01:22:47 -0400

    map

---
+++ js/map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic295aSIsImEiOiJjazF1eW45dG8wYmtrM2JwNWJ2NGczeGM0In0.u5zL-HKFNlawjUQCAGs6kQ';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	minZoom: 10,
+	maxZoom: 17,
+	style: 'mapbox://styles/soyi/ck23gjp3rfps61dpem51sitxb', 	
+	attributionControl: 'City of Charlottesville Open Data Portal (http://opendata.charlottesville.org/)'
+
+});
